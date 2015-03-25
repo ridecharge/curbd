cfc7966fb1d68d53c57fdf3f701be430af4aa14d
Stephen Garlick <sgarlick@taximagic.com>
2015-03-25 10:34:59 -0400

    mock-cf

--- curbd/curbd.py
+++ curbd/curbd.py
@@ -12,18 +12,29 @@ class Curbd(object):
 
     def from_json(self):
         program = self.options.program
-        with open("../curbd/" + program + "/" + self.service + ".json") as f:
-            for k, v in json.load(f).items():
-                print(k, v)
-                self.consul_conn.kv.put(program + "/" + self.service + "/" + k,
-                                        v)
+        key_prefix = program + "/" + self.service + "/"
+        path = "../curbd/" + program + "/" + self.service + ".json"
+        self.__from_json(path, key_prefix)
 
-    def from_cf(self):
-        key_prefix = self.service + "/" if self.service != 'env' else ""
+    def __from_json(self, path, key_prefix):
+        with open(path) as f:
+            for k, v in json.load(f).items():
+                print(key_prefix + k, v)
+                self.consul_conn.kv.put(key_prefix + k, v)
+
+    def __from_cf(self, key_prefix):
         for o in self.cf_conn.describe_stacks(self.env + "-" + self.service)[0].outputs:
             key = convert(o.key)
-            print(key, o.value)
+            print(key_prefix + key, o.value)
             self.consul_conn.kv.put(key_prefix + key, o.value)
+
+    def from_cf(self):
+        key_prefix = "cf/" + self.service + "/"
+        if self.options.local:
+            path = "../curbd/mock-cf/" + self.service + ".json"
+            self.__from_json(path, key_prefix)
+        else:
+            self.__from_cf(key_prefix)
 
 
 def convert(key):
